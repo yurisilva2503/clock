99ef63b7af3bc23bb72db28266a5f20ca6c0f48d
yurisilva2503 <yurigsfer@gmail.com>
2024-02-23 22:11:39 -0300

    Code redundance fix

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ setInterval(getHours, 1000);
 function getHours(){
     var clock = new Date();
 
-    var hour = clock.getHours();
-    var minutes = clock.getMinutes();
-    var seconds = clock.getSeconds();
-
     var hour = formatTimeUnit(clock.getHours());
     var minutes = formatTimeUnit(clock.getMinutes());
     var seconds = formatTimeUnit(clock.getSeconds());
