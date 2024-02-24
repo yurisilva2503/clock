fc08659ca5d39fec6db19e4b9963455698bde805
yurisilva2503 <yurigsfer@gmail.com>
2024-02-24 00:26:34 -0300

    Final project

--- index.js
+++ index.js
@@ -7,6 +7,25 @@ function getHours(){
     var minutes = formatTimeUnit(clock.getMinutes());
     var seconds = formatTimeUnit(clock.getSeconds());
 
+    var time = dayPeriod(hour);
+
+    switch (time){
+        case "day":
+            document.getElementById("body").style.backgroundImage = "url('./images/background-day.jpg')";
+            document.getElementById("title").style.color = "#e6e200"
+            break;
+        case "afternoon":
+            document.getElementById("body").style.backgroundImage = "url('./images/background-afternoon.jpg')";
+            document.getElementById("title").style.color = "#ff9100"
+            break;
+        case "night":
+            document.getElementById("body").style.backgroundImage = "url('./images/background-night.jpg')";
+            document.getElementById("title").style.color = "#ff004c"
+            break;
+        default:
+            break;
+    }
+
     document.getElementById("hour").innerHTML = hour + ":";
     document.getElementById("minutes").innerHTML = minutes + ":";
     document.getElementById("seconds").innerHTML = seconds;
@@ -14,4 +33,14 @@ function getHours(){
 
 function formatTimeUnit(unit) {
     return unit < 10 ? "0" + unit : unit;
+}
+
+function dayPeriod(value){
+    if (value >= 5 && value < 12){
+        return "day";
+    } else if (value >= 12 && value < 18){
+        return "afternoon"
+    } else {
+        return "night";
+    }
 }
